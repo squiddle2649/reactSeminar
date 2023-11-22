cc01b08dbf1fcb1b14c1e0924649b48e79a61e0b
Rafael Avelar <bruno.abj2017@gmail.com>
2023-11-22 10:04:28 +0100

    added hover styling for the main page buttons

--- src/pages/mainPage/twoButtons.js
+++ src/pages/mainPage/twoButtons.js
@@ -1,32 +1,20 @@
 import { Link } from 'react-router-dom';
-
-function TwoButtons(props){
-    const buttonStyle = {
-      fontFamily:window.appleFont,
-      color:"white",
-      backgroundColor:"black",
-      borderRadius: "10px",
-      width:"45%",
-      height:"45vh",
-      margin:"20px",
-      display:"flex",
-      justifyContent:"center",
-      alignItems:"center",
-      textDecoration: "none"
-
-    } 
+import './mainPageStyling/twoButtonsStyling.css'
+function TwoButtons(){
+  
     const containerStyle = {
       display:"flex",
       justifyContent:"center",
       alignItems:"center",
-      
-  
     }
+    
+
+    
     return <div className = "container" style = {containerStyle}>
-      <Link to="/berufskunde"style={buttonStyle}>
+      <Link to="/berufskunde" className="button berufButton">
         <h1>Berufskunde</h1>
       </Link>
-      <Link to ="/filmprojekt" style={buttonStyle}>
+      <Link to ="/filmprojekt" className="button filmButton">
         <h1>Filmprojekt</h1>
       </Link>
     </div>
